88b81a4c2e66115e458524dd27f306297e69dcca
Mohamed M. Abd Elhafiz <mabdelhafiz@deemalab.com>
2019-09-23 16:30:19 +0200

    renamed ui files

--- app/src/main/java/com/dlctt/mvvmlearning/ui/tasks/TasksActivity.kt
+++ app/src/main/java/com/dlctt/mvvmlearning/ui/tasks/TasksActivity.kt
@@ -1,13 +1,13 @@
-package com.dlctt.mvvmlearning.ui.main
+package com.dlctt.mvvmlearning.ui.tasks
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.dlctt.mvvmlearning.R
 
-class MainActivity : AppCompatActivity() {
+class TasksActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_tasks)
     }
 }
