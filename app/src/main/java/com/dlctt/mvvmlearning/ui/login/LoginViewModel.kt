f2ed6e3caff037597340ef54c309c9f95159e7b5
Mohamed M. Abd Elhafiz <mabdelhafiz@deemalab.com>
2019-10-27 17:25:18 +0200

    removed redundant code

--- app/src/main/java/com/dlctt/mvvmlearning/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/dlctt/mvvmlearning/ui/login/LoginViewModel.kt
@@ -2,7 +2,6 @@ package com.dlctt.mvvmlearning.ui.login
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dlctt.mvvmlearning.model.DTO.Result
 import com.dlctt.mvvmlearning.model.DTO.User
@@ -12,14 +11,9 @@ import com.dlctt.mvvmlearning.utils.BaseViewModel
 import com.dlctt.mvvmlearning.utils.Event
 import com.dlctt.mvvmlearning.utils.ServiceLocator
 import com.dlctt.mvvmlearning.utils.parseException
-import io.reactivex.SingleObserver
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
 import kotlinx.coroutines.launch
 
 class LoginViewModel : BaseViewModel() {
-    private val compositeDisposable: CompositeDisposable by lazy { CompositeDisposable() }
-
     private val loginRepo: LoginDataSource by lazy {
         ServiceLocator.getInstance().loginRepo
     }
@@ -45,31 +39,11 @@ class LoginViewModel : BaseViewModel() {
     }
 
     fun login(userId: String) {
-
         handleResult(Result.Loading())
         viewModelScope.launch {
             val result = loginRepo.loginById(userId.toInt())
             handleResult(result)
         }
-/*.subscribe(object : SingleObserver<List<User>> {
-            override fun onSuccess(usersList: List<User>) {
-                val result: Result<List<User>> = if (usersList.isEmpty())
-                    Result.Error(Exception("Wrong user id"))
-                else
-                    Result.Success(usersList)
-
-                handleResult(result)
-            }
-
-            override fun onSubscribe(d: Disposable) {
-                compositeDisposable.add(d)
-                handleResult(Result.Loading())
-            }
-
-            override fun onError(e: Throwable) {
-                handleResult(Result.Error(e))
-            }
-        })*/
     }
 
     private fun handleResult(result: Result<List<User>>) {
@@ -79,13 +53,9 @@ class LoginViewModel : BaseViewModel() {
             dialogMessage.value = Event(result.exception.parseException())
 
         if (result.isSucceeded()) {
-            if (result.data!!.isEmpty()) {
-                dialogMessage.value = Event("Wrong user id")
-            } else {
-                UserSession.userId = result.data[0].id
-                toastMessage.value = Event("Logged in successfully as ${result.data[0].name}")
-                navigateToMain.value = Event(true)
-            }
+            UserSession.userId = result.data!![0].id
+            toastMessage.value = Event("Logged in successfully as ${result.data[0].name}")
+            navigateToMain.value = Event(true)
         }
     }
 }
